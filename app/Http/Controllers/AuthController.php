bd24a05e0cf971e3827962016787681940b03a33
Will Corvo <nick.neytron@yandex.ru>
2025-10-04 04:24:41 +0300

    changed redirect after login to root

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
 
 		if (Auth::attempt($credentials)) {
 			$request->session()->regenerate();
-			return redirect('/tickets');
+			return redirect('/');
 		}
 
 		return back()->withErrors([
@@ -27,7 +27,6 @@ class AuthController extends Controller
 	}
 
 	public function logout(Request $request) {
-		// if (!Auth::check()) return redirect('/');
 		Auth::logout();
 		$request->session()->invalidate();
 		$request->session()->regenerateToken();
